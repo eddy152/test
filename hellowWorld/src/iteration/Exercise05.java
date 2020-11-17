96b95946169c06755f1f0c36a5e6d9f8c4f5c0f9
admin <admin@YD03-15>
2020-11-17 11:20:30 +0900

    ver2

--- hellowWorld/src/iteration/Exercise05.java
+++ hellowWorld/src/iteration/Exercise05.java
@@ -2,7 +2,17 @@ package iteration;
 
 public class Exercise05 {
 	public static void main(String[] args) {
-		
+
+		for (int i = 1; i <= 10; i++) {
+			int x = 4 * i;
+			for (int z = 1; z <= 10; z++) {
+				int y = 5 * z;
+				if (x + y == 60) {
+					System.out.println("(" + i + "," + z + ")");
+				}
+
+			}
+		}
 	}
 
 }
